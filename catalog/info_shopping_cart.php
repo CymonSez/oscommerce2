2f47264a0efd850f71f4814590714a7a0e7c23a5
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-02-17 20:37:26 +0100

    update doctype
    update body tag
    
    Move html layout to template_top.php/template_bottom.php; introduce dynamic per-file header tags modules
    
    Squashed commit of the following:
    
    commit 729517bd44bcc11d4d2f7734b315357cfe798b05
    Author: Harald Ponce de Leon <hpdl@oscommerce.com>
    Date:   Fri Mar 5 16:14:00 2010 +0100
    
        sanitize title
    
    commit 1d8110f65786c95af5522d7167bacea804ed33ab
    Author: Harald Ponce de Leon <hpdl@oscommerce.com>
    Date:   Fri Mar 5 14:44:46 2010 +0100
    
        new modular header tags feature integrated into template structure
    
    commit a6fa7094cc8734c66b33fc2ce143baa853d61243
    Author: Harald Ponce de Leon <hpdl@oscommerce.com>
    Date:   Sun Feb 21 15:13:34 2010 +0100
    
        revert header.php and footer.php for compatibility reasons, introduce template_top.php and template_bottom.php to define the html layout
    
    commit 4729ccaa57c26bc161d046540c8dd23aff2e413e
    Author: Harald Ponce de Leon <hpdl@oscommerce.com>
    Date:   Sun Feb 21 01:05:11 2010 +0100
    
        move html layout to header.php and footer.php
    
    introduce new header tag modules to add the category name and manufacturer name to the page title
    
    Allow global header tag modules to be loaded for all pages
    
    Add OpenSearch global header tag module
    
    move header tag modules to main directory; load all instead of individually based on request
    
    rename modules
    
    convert header tag modules to proper modules; these are now loaded on each page request; modules for specific pages need to be developed that way (checking against $PHP_SELF)
    
    Add a Google Analytics header tag module
    
    fix typo
    
    Update OpenSearch header tag module with parameters
    
    use favicon.ico as default icon
    
    Add E-Commerce Tracking
    
    Fix parameter title

--- catalog/info_shopping_cart.php
+++ catalog/info_shopping_cart.php
@@ -16,7 +16,7 @@
 
   require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_INFO_SHOPPING_CART);
 ?>
-<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html <?php echo HTML_PARAMS; ?>>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
